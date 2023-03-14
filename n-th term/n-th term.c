368f6dbee9be50e3342f47c0a906ffbfc4fd607d
joetemesi <joemunyendo@gmail.com>
2023-03-14 17:42:07 +0300

    Added logical flow comments

--- n-th term/n-th term.c
+++ n-th term/n-th term.c
@@ -14,8 +14,10 @@ int main()
     n=9;
     d=2;
     printf("Display the sequence: %d,%d,%d,%d,%d,%d,%d,%d,%d \n",num1,num2,num3,num4,num5,num6,num7,num8,num9);
+    //calculates nth term
     an=num1+(n-1)*d;
     printf("The last digit in the arithmetic sequence is: %d \n",an);
+    //Difference between terms
     d= (an-num1)/(n-1);
     printf("The difference of the numbers of the sequence is: %d \n",d);
     return 0;
