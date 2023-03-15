584c3ffbd5b4d69c5752d873040ea56ff6a653fc
joetemesi <joemunyendo@gmail.com>
2023-03-15 12:24:07 +0300

    Added function to take input from user

--- n-th term/n-th term.c
+++ n-th term/n-th term.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
+//nth term = num1+(terms-1)*difference
 int main()
 {
-    int num1,num2,num3,num4,num5,num6,num7,num8,num9,an,n,d;
-    num1=1;
-    num2=3;
-    num3=5;
-    num4=7;
-    num5=9;
-    num6=11;
-    num7=13;
-    num8=15;
-    num9=17;
-    n=9;
-    d=2;
-    printf("Display the sequence: %d,%d,%d,%d,%d,%d,%d,%d,%d \n",num1,num2,num3,num4,num5,num6,num7,num8,num9);
-    //calculates nth term
-    an=num1+(n-1)*d;
-    printf("The last digit in the arithmetic sequence is: %d \n",an);
-    //Difference between terms
-    d= (an-num1)/(n-1);
-    printf("The difference of the numbers of the sequence is: %d \n",d);
-    return 0;
+    //declare and get variables
+    int num1,terms,difference,nth;
+    printf("What is the first number in the arithmetic:");
+    scanf("%d",&num1);
+    printf("What is the number of terms in the sequence: ");
+    scanf("%d",&terms);
+    printf("What is the difference between the terms in the sequence:");
+    scanf("%d",&difference);
+    //arithmetic sequence formula
+    nth = num1+(terms-1)*difference;
+    printf("The nth term in the series is: %d \n",nth);
 
+    return 0;
 }
